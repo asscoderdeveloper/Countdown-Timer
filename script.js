515773cd6a384481299a1978dda589055996e31f
Alex Sánchez <alexsanchez@Air-de-Alex.home>
2020-09-11 00:22:44 +0200

    Finish

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "21 Noviembre 2020";
+const newYears = "1 Jan 2021";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
